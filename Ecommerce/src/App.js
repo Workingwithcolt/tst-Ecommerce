9402bc2ce65c7a8135c0ee361afef1cb607e1777
WorkingwithColt <mohitechetan2019@gmail.com>
2024-04-02 12:40:11 +0530

    updated

--- Ecommerce/src/App.js
+++ Ecommerce/src/App.js
@@ -14,23 +14,6 @@ function App() {
 
   return (
     <>
-      {/* <Auth dashboard={<Dashboard />} login={
-      <div className="flex justify-center items-center h-96">
-        <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          {
-            toggle ?
-              <LoginWithEmail />
-              : <SignUpWithEmail />
-          }
-          <Button
-            buttonName={toggle ? "Go To Register" : "Go To Login"}
-            type="button"
-            onPress={() => setToggle(!toggle)}
-          />
-
-        </div>
-      </div>
-    }></Auth> */}
       <Dashboard />
     </>
   );
